304d72ecbe7efd90700bdec1ad31aee2dec0792d
your username <2360881524@qq.com>
2023-08-31 19:05:51 +0800

    888

--- alpha/practice.py
+++ alpha/practice.py
@@ -8,7 +8,13 @@
 # 冒泡排序法
 lst = [2, 1, 1, 2, 3, 3, 4, 6, 2, 1, 7, 2, 8]
 for i in range(len(lst)-1):
+
+
     for j in range(len(lst)-1-i):
         if lst[j] > lst[j+1]:
             lst[j], lst[j+1] = lst[j+1], lst[j]
 print(lst)
+
+
+
+
